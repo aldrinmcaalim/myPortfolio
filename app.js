cb2ed98cc9485b5214e4acf2b1f331073a9f0ecb
Aldrin <aldrinmcaalim@gmail.com>
2021-03-22 00:28:28 -0400

    Click event to create element in DOM

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 // button.addEventListener('click', () => {
 //     console.log('you clicked me');
 // });
+const ul = document.querySelector('ul');
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = 'something new to do';
+    ul.prepend(li);
+});
 
 const items = document.querySelectorAll('li');
 
@@ -10,6 +18,7 @@ items.forEach(item => {
     item.addEventListener('click', e => {
         //console.log('item clicked');
         // console.log(e.target);
-        e.target.style.textDecoration = 'line-through';
+        // e.target.style.textDecoration = 'line-through';
+        e.target.remove();
     });
 });
